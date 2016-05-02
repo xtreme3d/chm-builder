a76bafeb343743286891679ea28b0049ae96deb3
Timur Gafarov <gecko0307@gmail.com>
2016-05-02 12:55:30 +0300

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# chm-builder
-Cross-platform CHM generator
+CHM Builder
+===========
+A simple cross-platform tool for generating files in MS Compiled HTMLHelp format (CHM). Written in Object Pascal, compiles with FPC/Lazarus. Fully self-contained (doesn't require installation of HTML Help Workshop).
+
+Usage
+-----
+`chmbuilder inputDir startPage.html TOC.hhc Index.hhk outputFile.chm [LocaleID]`
+
+* `inputDir` - input directory which contents will be included to CHM file
+* `startPage.html` - default page file (relative to inputDir)
+* `TOC.hhc` - table of contents file
+* `Index.hhk` - index file
+* `outputFile.chm` - output file
+* `LocaleID` (optional) - CHM language hex code (LCID). Default is English - `$0409`. Russian, for example, is `$0419`. Full list can be found here: https://msdn.microsoft.com/en-us/goglobal/bb964664.aspx
+
+Credits and license
+-------------------
+This project is based on CHMFileWriter by Andrew Haines. The code is distributed under GNU LGPL.
---
+++ main.pas
@@ -0,0 +1,65 @@
+program main;
+
+uses Classes, SysUtils, chmfilewriter;
+
+var
+  proj: TChmProject;
+  fs: TFileStream;
+
+procedure FindAllFilesInDir(strings: TStrings; dirname: String; chmName: String);
+var
+  SR: TSearchRec;
+  name: String;
+  name2: String;
+begin
+  if FindFirst(
+     IncludeTrailingBackslash(dirname) + '*', 
+     faAnyFile, SR) = 0 then
+  begin
+    repeat
+      name := IncludeTrailingBackslash(dirname) + SR.Name;
+      if Length(chmName) <> 0 then
+        name2 := chmName + '/' + SR.Name
+      else
+        name2 := SR.Name;
+      writeln(name2);
+      if (SR.Attr and faDirectory) = 0 then
+      begin
+        strings.Add(name2);
+      end
+      else if (SR.Name <> '.') and (SR.Name <> '..') then
+        FindAllFilesInDir(strings, name, name2);
+    until FindNext(Sr) <> 0;
+    FindClose(SR);
+  end;
+end;
+
+begin
+  if ParamCount < 5 then 
+  begin
+    writeln('Usage:');
+    writeln(' ' + ExtractFileName(ParamStr(0)) + 
+      ' inputDir indexFile hhcFile hhkFile outputFile [LocaleID]');
+    Exit;
+  end;
+
+  proj := TChmProject.Create;
+  proj.Title := 'Xtreme3D v3 Help';
+  proj.MakeSearchable := true;
+  proj.AutoFollowLinks := true;
+  proj.ProjectDirectory := ParamStr(1); //'html';
+  proj.DefaultPage := ParamStr(2); //'xtreme3d.htm';
+  proj.TableOfContentsFileName := ParamStr(3); //'html/TOC.hhc';
+  proj.IndexFileName := ParamStr(4); //'html/Index.hhk';
+  proj.OutputFileName := ParamStr(5); //'output.chm';
+
+  if ParamCount = 6 then
+    proj.LocaleID := StrToInt(ParamStr(6)); // default is English, $0419 = Russian
+
+  FindAllFilesInDir(proj.Files, 'html', '');
+
+  fs := TFileStream.Create(proj.OutputFileName, fmCreate);
+  proj.WriteChm(fs);
+  fs.Free;
+end.
+
